bb22a3b26d621496988b8b9a080c4022d800c097
zulfiqarlaili <zulfiqarlaili@gmail.com>
2024-03-04 03:23:38 +0800

    chore: update project

--- src/partials/ProjectList.tsx
+++ src/partials/ProjectList.tsx
@@ -16,6 +16,36 @@ const ProjectList = () => (
   >
     <div className="flex flex-col gap-6">
       <Project
+        name="Paste 2 Download"
+        description="Paste 2 Download is a web application that help to remove TikTok video watermarks."
+        link="https://paste2download.com"
+        img={{
+          src: '/assets/images/paste2download.jpg',
+          alt: 'Paste 2 Download',
+        }}
+        category={
+          <>
+            <Tags color={ColorTags.ORANGE}>Svelte</Tags>
+            <Tags color={ColorTags.SKY}>Tailwind</Tags>
+            <Tags color={ColorTags.AMBER}>Shadcn UI</Tags>
+            <Tags color={ColorTags.EMERALD}>FastAPI</Tags>
+          </>
+        }
+      />
+      <Project
+        name="Tak Sedap"
+        description="'Tak Sedap' guides you from bad street food. Share your finds, let's enhance our street food experience together!"
+        link="https://taksedap.vercel.app"
+        img={{ src: '/assets/images/taksedap.jpg', alt: 'Tak Sedap' }}
+        category={
+          <>
+            <Tags color={ColorTags.ORANGE}>Svelte</Tags>
+            <Tags color={ColorTags.SKY}>Tailwind</Tags>
+            <Tags color={ColorTags.EMERALD}>Supabase</Tags>
+          </>
+        }
+      />
+      <Project
         name="Born Date"
         description="Based on numerology given by your birth date. We used it to calculate the elements for you personality. Generate an image as a guide for you to discover your strength and weakness"
         link="https://borndate.web.app"
@@ -34,7 +64,7 @@ const ProjectList = () => (
         name="Just Type"
         description="Are you having trouble to WhatsApp someone but you don`t want to save their number? Just type their number and hit enter."
         link="/"
-        img={{ src: '/assets/images/justtype.jpg', alt: 'JustType' }}
+        img={{ src: '/assets/images/just-type.jpg', alt: 'JustType' }}
         category={
           <>
             <Tags color={ColorTags.RED}>Angular.js</Tags>
